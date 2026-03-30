5bb00d859941178d8a2470ab61b7996ba9985a25
Phil Howson <philjhowson@outlook.com>
2026-03-30 16:38:15 +0000

    updating .sql

--- models/marts/monthly_kpis.sql
+++ models/marts/monthly_kpis.sql
@@ -1,13 +1,15 @@
 /*
 Groups the joined table 'product_quantity' by month
-to calculate the average quantity per order per month.
-I kept 3 decimal places for percision in any downstream
+to calculate the average quantity per order per month
+and the total products sold per month. I kept the
+average 3 decimal places for percision in any downstream
 tasks, even though you can't have a part of a product
 in a real order.
 */
 
 select format_date('%Y-%m', order_date) as month,
-  round(avg(qty_product), 3) as average_quantity
+  round(avg(qty_product), 3) as average_quantity,
+  count(*) as total_orders
 from {{ref('product_quantity')}}
 where order_date >= '2023-01-01'
     and order_date < '2024-01-01'
